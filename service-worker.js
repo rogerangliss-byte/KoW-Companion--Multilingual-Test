9ea50bbecf68c20fa3e653b37d3ee81e5f4d1e12
FireStorm <roger.angliss@gmail.com>
2026-08-15 20:51:41 +0100

    Cache complete v4.4.0 multilingual language sources

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE='kow-multilingual-v4.4.0-test5-source-language-sync';
+const CACHE='kow-multilingual-v4.4.0-test6-language-source-complete';
 const ASSETS=[
   './','./index.html','./manifest.json','./icon.svg','./background-portrait.jpg','./background-landscape.jpg',
   './officers.csv','./officers.json','./en.js','./fr.js','./de.js','./it.js',
+  './lang/en.js','./lang/fr.js','./lang/de.js','./lang/it.js','./lang/v440-release-sync.js','./lang/v440-language-qa.js',
   './USER-GUIDE.md','./USER-GUIDE-fr.md','./USER-GUIDE-de.md','./USER-GUIDE-it.md','./qa-v440.js'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
