76c46c659ba317ad5091bebbed77d1a7107839b2
FireStorm <roger.angliss@gmail.com>
2026-08-27 14:12:58 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kow-multilingual-test-v4.6.0-qa15-full-reconcile-20260827';
+const CACHE='kow-multilingual-test-v4.6.0-qa16-rendered-app-closure-20260827';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil(
